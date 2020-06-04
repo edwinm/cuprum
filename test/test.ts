22f22d40c8ca23913c6a347320f154bf8b472cec
Edwin Martin <edwin@bitstorm.org>
2020-06-04 12:08:46 +0200

    Added more unit tests

--- test/test.ts
+++ test/test.ts
@@ -31,4 +31,64 @@ describe("Cuprum", () => {
 
     pipe1$.dispatch("a1");
   });
+
+  it("Unsubscribe", () => {
+    const pipe1$ = new Cuprum<string>();
+
+    pipe1$.subscribe(sub);
+
+    pipe1$.dispatch("a1");
+
+    pipe1$.unsubscribe(sub);
+
+    pipe1$.dispatch("a2");
+
+    function sub(value) {
+      assert.equal(value, "a1");
+    }
+  });
+
+  it("Two subscribers", () => {
+    let count1 = 0;
+    let count2 = 0;
+    const pipe1$ = new Cuprum<string>();
+
+    pipe1$.subscribe(sub1);
+    pipe1$.subscribe(sub2);
+
+    pipe1$.dispatch("a1");
+
+    pipe1$.unsubscribe(sub1);
+
+    pipe1$.dispatch("a1");
+
+    pipe1$.unsubscribe(sub2);
+
+    pipe1$.dispatch("a1");
+
+    assert.equal(count1, 1);
+    assert.equal(count2, 2);
+
+    function sub1(value) {
+      assert.equal(value, "a1");
+      count1++;
+    }
+
+    function sub2(value) {
+      assert.equal(value, "a1");
+      count2++;
+    }
+  });
+
+  it("Promise", (done) => {
+    const pipe1$ = new Cuprum<string>();
+
+    (async function () {
+      setTimeout(() => pipe1$.dispatch("a2"), 10);
+
+      const value = await pipe1$.promise();
+      assert.equal(value, "a2");
+      done();
+    })();
+  });
 });
