6c87e5de0f533af3aa34e7027c9a066d312de9d5
Edwin Martin <edwin@bitstorm.org>
2020-06-09 18:29:27 +0200

    v0.1.9

--- dist/cuprum.d.ts
+++ dist/cuprum.d.ts
@@ -20,7 +20,13 @@ export declare class Cuprum<T> {
 }
 export declare function tap(fn: (val: any) => any): any;
 export declare function fromEvent(element: any, eventType: any): Cuprum<Event>;
+export declare function combine<T>(obs1$: Cuprum<T>): Cuprum<[T]>;
 export declare function combine<T, U>(obs1$: Cuprum<T>, obs2$: Cuprum<U>): Cuprum<[T, U]>;
+export declare function combine<T, U, V>(obs1$: Cuprum<T>, obs2$: Cuprum<U>, obs3$: Cuprum<V>): Cuprum<[T, U, V]>;
+export declare function combine<T, U, V, W>(obs1$: Cuprum<T>, obs2$: Cuprum<U>, obs3$: Cuprum<V>, obs4$: Cuprum<W>): Cuprum<[T, U, V, W]>;
+export declare function combine<T, U, V, W, X>(obs1$: Cuprum<T>, obs2$: Cuprum<U>, obs3$: Cuprum<V>, obs4$: Cuprum<W>, obs5$: Cuprum<X>): Cuprum<[T, U, V, W, X]>;
+export declare function combine<T, U, V, W, X, Y>(obs1$: Cuprum<T>, obs2$: Cuprum<U>, obs3$: Cuprum<V>, obs4$: Cuprum<W>, obs5$: Cuprum<X>, obs6$: Cuprum<Y>): Cuprum<[T, U, V, W, X, Y]>;
+export declare function combine<T, U, V, W, X, Y, Z>(obs1$: Cuprum<T>, obs2$: Cuprum<U>, obs3$: Cuprum<V>, obs4$: Cuprum<W>, obs5$: Cuprum<X>, obs6$: Cuprum<Y>, obs7$: Cuprum<Z>): Cuprum<[T, U, V, W, X, Y, Z]>;
 interface Subscription {
     unsubscribe: () => void;
 }
