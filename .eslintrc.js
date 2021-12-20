37aa1097cd2c99e5d47a9835db570ed77e42d86c
Edwin Martin <edwin@bitstorm.org>
2021-12-20 16:47:29 +0100

    Improve linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,7 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/ban-types": "off",
     "no-prototype-builtins": "off",
